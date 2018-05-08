2bf5821a6c90c47c371938306d8d94461a7f16ac
Vincent Au <vincent.au@anu.edu.au>
2018-05-09 09:38:41 +1000

    Add the bigram data to raw probs

--- word_level.py
+++ word_level.py
@@ -416,13 +416,13 @@ class WordFeatures(object):
         probs = self.log_probs.ngram_raw_probs(1, sentence)
         res = [[p] for p in probs]
 
-#       # get bigram data, raw probs
-#       if self.ngrams >= 2:
-#           bigrams = self.log_probs.ngram_raw_probs(2, sentence)
-#           left_bigrams = [probs[0]] + bigrams
-#           right_bigrams = bigrams + [probs[-1]]
-#           for i, (l, r) in enumerate(zip(left_bigrams, right_bigrams)):
-#               res[i].extend([min(l,r), max(l, r)])
+        # get bigram data, raw probs
+        if self.ngrams >= 2:
+            bigrams = self.log_probs.ngram_raw_probs(2, sentence)
+            left_bigrams = [probs[0]] + bigrams
+            right_bigrams = bigrams + [probs[-1]]
+            for i, (l, r) in enumerate(zip(left_bigrams, right_bigrams)):
+                res[i].extend([min(l,r), max(l, r)])
 
         return [SimpleRowSparse(p) for p in res]
 
